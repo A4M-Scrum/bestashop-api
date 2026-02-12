51a9f5b658e7516f9f66b4a70fdabc2afee30051
Angel <angelspot2002@gmail.com>
2026-02-12 10:22:33 +0100

    Corregido archivo de SQL

--- bestashop_db.sql
+++ bestashop_db.sql
@@ -1,6 +1,29 @@
-CREATE DATABASE bestashop_db if not EXISTS;
+CREATE DATABASE IF NOT EXISTS bestashop_db;
+USE bestashop_db;
+
+CREATE TABLE categories (
+    category_id INT AUTO_INCREMENT,
+    name VARCHAR(45),
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE sellers (
+    seller_id INT  AUTO_INCREMENT,
+    name VARCHAR(45),
+    PRIMARY KEY (seller_id)
+);
+
 CREATE TABLE products (
-    product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT AUTO_INCREMENT,
+    seller_id INT,
+    category_id INT,
     description VARCHAR(512),
-    price 
-)
+    price DECIMAL(10, 2),
+    location VARCHAR(100),
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (seller_id) REFERENCES sellers(seller_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+
+SELECT * FROM products;
