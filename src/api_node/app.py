17d4d25f210a3ae49a9bf48d5c5a69d9297465be
Plush <angelspot2002@gmail.com>
2026-03-03 13:48:37 +0100

    Completed formatting of values from SELECT queries Completed connection of endpoint and API Created second endpoint for accessing categories Added exception handling to root endpoint

--- src/api_node/app.py
+++ src/api_node/app.py
@@ -1,18 +1,33 @@
 from flask import Flask, jsonify, request
 
+from api_node.database import getall_categories
+from database import getall_products, bshop_db
+
 app = Flask(__name__)
 
+database = bshop_db()
+database.set_conn(user = 'remote',host = '10.3.3.205', database = 'bestashop_db')
 @app.route("/")
 def home():
     if request.method == "GET":
-        diction = {
-            1 : "hello world",
-            2 : ["a!",True,22.32]
-        }
-        return jsonify(diction)
+        try:
+            products = getall_products(database)
+            return jsonify(products)
+        except:
+            return "Oops, something went wrong! Please, try again later."
+    else: return "Hello world"
+
+@app.route("/categories")
+def categories_endpoint():
+    if request.method == "GET":
+        try:
+            products = getall_products(database)
+            return jsonify(products)
+        except:
+            return "Oops, something went wrong! Please, try again later."
     else: return "Hello world"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 print("hello world")
--- src/api_node/database.py
+++ src/api_node/database.py
@@ -1,11 +1,12 @@
 from mysql import connector
 from dataclasses import dataclass
 
+
 @dataclass
 class bshop_db:
-    _connection: list|None = None
+    _connection: list | None = None
 
-    def set_conn(self,**kwargs) -> None:
+    def set_conn(self, **kwargs) -> None:
         '''
         Configura la conexion con la base de datos que usará esta clase
         
@@ -15,60 +16,92 @@ class bshop_db:
             host=kwargs['host'],
             user=kwargs['user'],
             database=kwargs['database'],
-            )
+        )
         cursor_db = database.cursor()
 
         if self._connection != None:
             self._connection['database'].close()
             self._connection['cursor'].close()
 
-        self._connection = {'database':database,'cursor':cursor_db}
-    
-    def stm_select(self,columns: str,tables: str,where: str|None = None,order_by: str|None = None):
+        self._connection = {'database': database, 'cursor': cursor_db}
+
+    def stm_select(self, columns: str, tables: str, where: str | None = None, order_by: str | None = None):
         cursor = self._connection['cursor']
         statement = f'SELECT {columns} FROM {tables}'
         if where: statement += f' WHERE {where}'
         if order_by: statement += f' ORDER BY {order_by}'
         statement += ';'
 
-        return cursor.execute(statement)
-    
-    def stm_insert_into(self, table: str,values: list,columns: list|None = None):
-        sql = "INSERT INTO %s(%s) VALUES (%s);"
-        val = (table, str(columns)[2:-2], str(values)[2:-2])
-        print('sql = ' + sql)
-        print('columns = ' + str(columns))
-        print('values:')
-        for v in val:
-            print(v)
-        cursor = self._connection['cursor']
-        cursor.execute(sql, val)
-        self._connection['database'].commit()
+        cursor.execute(statement)
 
+        # Get the results of the query
+        results = cursor.fetchall()
+
+        # Get the information of the columns
+        columns = cursor.description
+
+        # Assign each item the name of the column
+        # The result would be: "name_of_the_column": "value_of_the_column"
+        # Do this for each column of each item
+        # Probably a bad and inefficient way to do this, but it works, so...
+        items = []
+        for r in results:
+            new_item = {}
+            for index, c in enumerate(columns):
+                new_item[c[0]] = r[index]
+            items.append(new_item)
+
+        return items
+
+    def stm_insert_into(self, table: str, values: list, columns: list | None = None):
+        cursor = self._connection['cursor']
+
+        columns_str = ','.join(columns)
+        sql = f'INSERT INTO {table}({columns_str}) VALUES ({str(values)[1:-1]});'
+        print(sql)
+        cursor.execute(sql)
+        self._connection['database'].commit()
 
     def stm_update(self, table: str, column_value: dict, where: str):
         statement = 'UPDATE ? SET ? WHERE ?;'
 
-    def stm_delete(self, table:str,where: str|None):
+    def stm_delete(self, table: str, where: str | None):
         statement = 'DELETE FROM ? WHERE ?;'
 
+
 def getall_products(conn: bshop_db):
-    products = conn.stm_select(columns='*',tables='products')
-    print(products)
+    products = conn.stm_select(columns='product_id, seller_id, category_id, name, description, price, image_url', tables='products')
+    return products
 
-def insert_product(conn: bshop_db, values: list):
-    conn.stm_insert_into(table='products', columns=['seller_id, category_id, name, description, price, image_url'], values=values)
 
-def insert_category(conn:bshop_db, values: list):
-    conn.stm_insert_into(table='categories', columns=['name'], values=values)
+def getall_categories(conn: bshop_db):
+    result = conn.stm_select(columns='*', tables='categories')
+
+    return result
+
+
+def insert_product(conn: bshop_db, seller_id: int, category_id: int, name: str, description: str, price: float,
+                   image_url: str):
+    conn.stm_insert_into(
+        table='products',
+        columns=['seller_id, category_id, name, description, price, image_url'],
+        values=[seller_id, category_id, name, description, price, image_url]
+    )
+
+
+def insert_category(conn: bshop_db, values: list):
+    conn.stm_insert_into(table='categories', values=values, columns=['name'])
+
 
 def insert_seller(conn: bshop_db, values: list):
-    conn.stm_insert_into(table='sellers', columns=['name'], values=values)
+    conn.stm_insert_into(table='sellers', values=values, columns=['name'])
 
 
 if __name__ == "__main__":
     database = bshop_db()
-    database.set_conn(user = 'remote',host = '10.3.3.205', database = 'bestashop_db')
-    insert_category(database, ['Coche'])
-    insert_seller(database, values=['Honda'])
+    database.set_conn(user='remote', host='10.3.3.205', database='bestashop_db')
+    # insert_category(database, ['Coche'])
+    # insert_seller(database, values=['Honda'])
+    insert_product(database, 2, 2, "Civic 2006", "Old reliable", 15000, "")
     test = getall_products(database)
+    print(test)
